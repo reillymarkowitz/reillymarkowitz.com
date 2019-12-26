154b25a8141b977517ad0498af916790117421c5
Reilly <reillymarkowitz@gmail.com>
2019-12-26 11:59:31 -0500

    Replaced navbar component with navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatCardModule } from '@angular/material';
 import { ProjectPageComponent } from './project-page/project-page.component';
@@ -13,17 +12,18 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { environment } from 'src/environments/environment';
 import { VideoBackgroundComponent } from './video-background/video-background.component';
 import { NameBoxComponent } from './name-box/name-box.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 const firebaseConfig: FirebaseOptions = environment.firebaseConfig;
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     ProjectCardComponent,
     ProjectPageComponent,
     VideoBackgroundComponent,
-    NameBoxComponent
+    NameBoxComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
