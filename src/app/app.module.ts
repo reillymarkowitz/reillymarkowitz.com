8c465993866d83a9967f51758c6a49143187576a
Reilly Markowitz <reillymarkowitz@gmail.com>
2020-03-22 16:24:07 -0400

    Removed unused project page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatCardModule } from '@angular/material';
-import { ProjectPageComponent } from './project-page/project-page.component';
 import { AngularFireModule, FirebaseOptions } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
@@ -18,7 +17,6 @@ const firebaseConfig: FirebaseOptions = environment.firebaseConfig;
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectPageComponent,
     VideoBackgroundComponent,
     NameBoxComponent,
     NavigationComponent
