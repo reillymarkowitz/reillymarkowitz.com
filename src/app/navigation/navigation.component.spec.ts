eec2c3b423316b6401b116c72cea458e01ec4697
Reilly Markowitz <reillymarkowitz@gmail.com>
2020-05-14 02:13:55 -0400

    updated navigation buttons

--- src/app/navigation/navigation.component.spec.ts
+++ src/app/navigation/navigation.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavigationComponent } from './navigation.component';
-import { By } from '@angular/platform-browser';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { AngularFireModule, FirebaseOptions } from '@angular/fire';
 import { environment } from 'src/environments/environment';
@@ -30,65 +29,5 @@ describe('NavigationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  describe('resume button', () => {
-    let resumeButton: HTMLAnchorElement;
-
-    beforeEach(() => {
-      resumeButton = fixture.debugElement.query(By.css('#resumeButton')).nativeElement;
-    });
-
-    it('should create', () => {
-      expect(resumeButton).toBeTruthy();
-    });
-
-    it('should display the correct text', () => {
-      expect(resumeButton.innerText).toBe('Resume');
-    });
-
-    it('should open link in a new tab when clicked', () => {
-      expect(resumeButton.target).toBe('_blank');
-    });
-  });
-
-  describe('GitHub button', () => {
-    let githubButton: HTMLAnchorElement;
-
-    beforeEach(() => {
-      githubButton = fixture.debugElement.query(By.css('#githubButton')).nativeElement;
-    });
-
-    it('should create', () => {
-      expect(githubButton).toBeTruthy();
-    });
-
-    it('should display the correct text', () => {
-      expect(githubButton.innerText).toBe('GitHub');
-    });
-
-    it('should open link in a new tab when clicked', () => {
-      expect(githubButton.target).toBe('_blank');
-    });
-  });
-
-  describe('LinkedIn button', () => {
-    let linkedinButton: HTMLAnchorElement;
-
-    beforeEach(() => {
-      linkedinButton = fixture.debugElement.query(By.css('#linkedinButton')).nativeElement;
-    });
-
-    it('should create', () => {
-      expect(linkedinButton).toBeTruthy();
-    });
-
-    it('should display the correct text', () => {
-      expect(linkedinButton.innerText).toBe('LinkedIn');
-    });
-
-    it('should open link in a new tab when clicked', () => {
-      expect(linkedinButton.target).toBe('_blank');
-    });
   });
 });
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -8,20 +8,43 @@ import { AngularFireStorage } from '@angular/fire/storage';
 })
 export class NavigationComponent implements OnInit {
   private resumeUrl: string;
+  private resumeIconUrl: string;
+  private githubIconUrl: string;
+  private linkedinIconUrl: string;
 
   constructor(private storage: AngularFireStorage) { }
 
   ngOnInit() {
-    const resumePath = 'Reilly_Markowitz.pdf';
-    const resumeRef = this.storage.ref(resumePath);
-    resumeRef.getDownloadURL().subscribe(
-      url => {
-        this.resumeUrl = url;
-      },
-      error => {
-        this.resumeUrl = '';
-      }
-    );
+    this.setResumeUrl();
+    this.setButtonIconUrls();
   }
 
+  isLoaded() {
+    const requiredFields = [ 
+      this.resumeUrl, 
+      this.resumeIconUrl, 
+      this.githubIconUrl, 
+      this.linkedinIconUrl 
+    ];
+    
+    return requiredFields.every(field => typeof(field) !== 'undefined');
+  }
+
+  setResumeUrl() {
+    this.setFieldToFileDownloadUrl('resumeUrl', 'Reilly_Markowitz.pdf');
+  }
+
+  setButtonIconUrls() {
+    this.setFieldToFileDownloadUrl('resumeIconUrl', 'button_icons/resume.png');
+    this.setFieldToFileDownloadUrl('githubIconUrl', 'button_icons/github.png');
+    this.setFieldToFileDownloadUrl('linkedinIconUrl', 'button_icons/linkedin.png');
+  }
+
+  setFieldToFileDownloadUrl(fieldName: string, filePath: string) {
+    const observable = this.storage.ref(filePath).getDownloadURL();
+    observable.subscribe(
+      url => { this[fieldName] = url; },
+      error => { this[fieldName] = ''; }
+    );
+  }
 }
